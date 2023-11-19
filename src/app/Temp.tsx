b40ccec5fb3784847819e2be4299b96917444f89
Stendhalsynd <qmflf556@naver.com>
2023-11-20 00:19:33 +0900

    docs: eslint + prettier 설정

---
+++ src/app/Temp.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Temp: React.FC = () => <p>test</p>
+
+export default Temp
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+
+import Temp from '@/app/Temp.tsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Temp />
       <body className={inter.className}>{children}</body>
     </html>
   )

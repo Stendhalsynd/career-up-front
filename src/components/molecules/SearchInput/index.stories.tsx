2d186aeb032b0cdc8809de92c967d0321c57d812
Stendhalsynd <qmflf556@naver.com>
2023-11-22 20:13:58 +0900

    feat/#17-searchInput 컴포넌트 생성

---
+++ src/components/molecules/SearchInput/index.stories.tsx
@@ -0,0 +1,33 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { ThemeProvider } from 'styled-components'
+import { SearchInput } from 'components/molecules/index.ts'
+import { theme } from 'themes/index.ts'
+
+const meta: Meta<typeof SearchInput> = {
+  title: 'Molecules/SearchInput',
+  component: SearchInput,
+  argTypes: {
+    variant: {
+      option: ['primarySmall', 'primaryLarge', 'skillSmall', 'skillLarge'],
+      control: { type: 'select' },
+      description: '검색 입력',
+      defaultValue: 'primarySmall',
+      table: {
+        type: { summary: '검색할 내용 입력' },
+      },
+    },
+  },
+}
+export default meta
+type Story = StoryObj<typeof SearchInput>
+
+export const PrimarySmall: Story = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+  // render: () => <SearchInput variant=''/>,
+}

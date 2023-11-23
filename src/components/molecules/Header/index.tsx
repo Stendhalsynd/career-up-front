398a7f2312e297d359e401c15eff6426bdd5b23e
dawncoding <sunnuri99@gmail.com>
2023-11-23 17:50:49 +0900

    mod/#6-header component 반응형 웹 적용

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,50 +1,50 @@
-// import styled from 'styled-components'
-import Icon from 'components/atoms/Icon/index.tsx'
-import Logo from 'components/atoms/Logo/index.tsx'
-import Text from 'components/atoms/Text/index.tsx'
-import Box from 'components/layout/Box/index.tsx'
+'use client'
+
+import { Icon, Logo, Text } from 'components/atoms/index.ts'
 import Flex from 'components/layout/Flex/index.tsx'
+
+/**
+ * 헤더
+ * @description
+ * 헤더 내 로고, 텍스트, 아이콘 컴포넌트들 가운데 정렬
+ * 반응형 웹 적용: viewport가 모바일 사이즈인 경우, 텍스트 컴포넌트 display: none / 901px부터 텍스트 컴포넌트 display: flex
+ * @example <Header />
+ */
 
 const Header = () => {
   return (
     <Flex
       backgroundColor={'black'}
       justifyContent={'space-between'}
-      height={'40px'}
+      alignItems={'center'}
+      height={{ base: '40px', md: '50px' }}
       padding={'10px'}
     >
       <Flex>
-        <Logo customWidth={50} customHeight={17} />
+        <Logo width={60} />
       </Flex>
 
-      <Flex justifyContent={'space-evenly'} width={'300px'}>
-        <Text
-          variant="small"
-          fontSize={{ base: 'extraSmall', md: 'small' }}
-          color={'white'}
-        >
+      <Flex
+        justifyContent={'space-evenly'}
+        width={'300px'}
+        display={{ base: 'none', md: 'flex' }}
+      >
+        <Text variant="small" fontSize={{ md: 'small' }} color={'white'}>
           멘토 찾기
         </Text>
 
-        <Text
-          variant="small"
-          fontSize={{ base: 'extraSmall', md: 'small' }}
-          color={'white'}
-        >
+        <Text variant="small" fontSize={{ md: 'small' }} color={'white'}>
           나의 채팅
         </Text>
 
-        <Text
-          variant="small"
-          fontSize={{ base: 'extraSmall', md: 'small' }}
-          color={'white'}
-        >
+        <Text variant="small" fontSize={{ md: 'small' }} color={'white'}>
           로그인
         </Text>
       </Flex>
-      <Box>
-        <Icon iconName="menu" width={24} height={24} />
-      </Box>
+
+      <Flex>
+        <Icon iconName="menu" width={28} height={28} />
+      </Flex>
     </Flex>
   )
 }

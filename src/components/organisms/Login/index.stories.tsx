189738e1d4792cc769c04301b1cf2e530f97b551
dawncoding <sunnuri99@gmail.com>
2023-11-24 20:15:41 +0900

    feat/#36-add login, signup component storybook

---
+++ src/components/organisms/Login/index.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { ThemeProvider } from 'styled-components'
+import Login from 'components/organisms/Login/index.tsx'
+import { theme } from 'themes/index.ts'
+
+const meta: Meta<typeof Login> = {
+  title: 'Organisms/Login',
+  component: Login,
+}
+
+export default meta
+type Story = StoryObj<typeof Login>
+
+export const Normal: Story = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+}
---
+++ src/components/organisms/Signup/index.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { ThemeProvider } from 'styled-components'
+import Signup from 'components/organisms/Signup/index.tsx'
+import { theme } from 'themes/index.ts'
+
+const meta: Meta<typeof Signup> = {
+  title: 'Organisms/Signup',
+  component: Signup,
+}
+
+export default meta
+type Story = StoryObj<typeof Signup>
+
+export const Normal: Story = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={theme}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+}

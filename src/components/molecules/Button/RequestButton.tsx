b6b2d338341bd8d808898f6098d7ee27de70f99d
dawncoding <sunnuri99@gmail.com>
2023-11-24 10:30:19 +0900

    mod-request button 코드 수정

--- src/components/molecules/Button/RequestButton.tsx
+++ src/components/molecules/Button/RequestButton.tsx
@@ -4,24 +4,24 @@ import PropTypes from 'prop-types'
 import React, { ReactNode } from 'react'
 //import Icon from 'components/atoms/Icon/index.tsx'
 import Button, { ButtonProps } from 'components/molecules/Button/index.tsx'
+import { Responsive } from 'types/styles'
 
 type RequestButtonProps = {
   iconName?: string
+  width?: Responsive<string>
+  height?: Responsive<string>
   children?: ReactNode
 } & ButtonProps
 
 const RequestButton: React.FC<RequestButtonProps> = ({
   // iconName,
+  width = { base: 'small' },
+  height = { base: 'small' },
   children,
   ...rest
 }) => {
   return (
-    <Button
-      variant="primary"
-      width={{ base: 'small' }}
-      height={{ base: 'small' }}
-      {...rest}
-    >
+    <Button variant="primary" width={width} height={height} {...rest}>
       {/* iconName이 있으면 아이콘 추가 */}
       {/* {iconName && <Icon iconName={iconName} width={20} height={20} />} */}
 

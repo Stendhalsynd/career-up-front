0c01a203b9296787129761081446c849c55c5fdc
dawncoding <sunnuri99@gmail.com>
2023-12-03 18:30:30 +0900

    bug: add <RecoilClient> inside WorkerInfoCard Component storybook

--- src/components/organisms/WorkerInfoCard/index.stories.tsx
+++ src/components/organisms/WorkerInfoCard/index.stories.tsx
@@ -1,8 +1,11 @@
 import { Meta, StoryObj } from '@storybook/react'
 import React from 'react'
+import { useSetRecoilState } from 'recoil'
 import { ThemeProvider } from 'styled-components'
 import WorkerInfoCard from 'components/organisms/WorkerInfoCard/index.tsx'
+import RecoilClient from 'lib/recoil.tsx'
 import { theme } from 'themes/index.ts'
+import { selectedNicknameState } from 'utils/state.ts'
 
 const meta: Meta<typeof WorkerInfoCard> = {
   title: 'Organisms/WorkerInfoCard',
@@ -14,11 +17,20 @@ export default meta
 
 type Story = StoryObj<typeof WorkerInfoCard>
 
+const WorkerInfoCardState = () => {
+  const setState = useSetRecoilState(selectedNicknameState)
+  setState('행복한 다람쥐')
+  return <WorkerInfoCard />
+}
+
 export const Normal: Story = {
+  render: () => <WorkerInfoCardState />,
   decorators: [
     (Story) => (
       <ThemeProvider theme={theme}>
-        <Story />
+        <RecoilClient>
+          <Story />
+        </RecoilClient>
       </ThemeProvider>
     ),
   ],
